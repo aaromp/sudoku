c43db9f276d90cefcf000419e6ba20c1b58b7048
Aaron Ward <aaromp@gmail.com>
2015-02-07 13:13:33 -0800

    add a gulp watch task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,29 +1,22 @@
 var gulp = require('gulp');
 
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var gulpif = require('gulp-if');
-var uglify = require('gulp-uglify');
-var streamify = require('gulp-streamify');
-
 var jade = require('gulp-jade');
 
 var stylus = require('gulp-stylus');
 var nib = require('nib');
 var sourcemaps = require('gulp-sourcemaps');
 
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
+
 var env = process.env.NODE_ENV || 'development';
 var devDir = 'build/dev';
 
-gulp.task('bundle', function() {
-	return browserify('./src/js/main', { debug: env === 'development' }).bundle()
-								.pipe(source('bundle.js'))
-								.pipe(streamify(gulpif(env === 'production', uglify())))
-								.pipe(gulp.dest('build/dev/js'));
-});
-
 gulp.task('jade', function() {
-	return gulp.src('src/templates/*.jade')
+	return gulp.src('src/templates/**/*.jade')
 			   .pipe(jade())
 			   .pipe(gulp.dest(devDir));
 });
@@ -35,8 +28,23 @@ gulp.task('stylus', function() {
 	if (env === 'production') config.compress = true;
 	else config.lineos = true;
 
-	return gulp.src('src/styles/*.styl')
+	return gulp.src('src/styles/**/*.styl')
 		       .pipe(stylus(config))
 		       .pipe(sourcemaps.write())
 		       .pipe(gulp.dest(devDir + '/css'));
 });
+
+gulp.task('bundle', function() {
+	return browserify('./src/js/main', { debug: env === 'development' }).bundle()
+								.pipe(source('bundle.js'))
+								.pipe(streamify(gulpif(env === 'production', uglify())))
+								.pipe(gulp.dest(devDir + '/js'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('src/templates/**/*.jade', ['jade']);
+	gulp.watch('src/styles/**/*.styl', ['stylus']);
+	gulp.watch('src/js/**/*.js', ['bundle']);
+});
+
+gulp.task('default', ['jade', 'stylus', 'bundle', 'watch']);

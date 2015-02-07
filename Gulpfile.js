e18eabc7225cc43df61ee62fc68dafcbdd1d0636
Aaron Ward <aaromp@gmail.com>
2015-02-07 12:34:13 -0800

    add gulpif for sourcemaps in development

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,17 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var gulpif = require('gulp-if'); // to get source maps in development
 var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify');
 var jade = require('gulp-jade');
 
+var env = process.env.NODE_ENV || 'development';
+
 gulp.task('bundle', function() {
-	return browserify('./src/js/main', { debug: true }).bundle()
+	return browserify('./src/js/main', { debug: env === 'development' }).bundle()
 								.pipe(source('bundle.js'))
-								.pipe(streamify(uglify()))
+								.pipe(streamify(gulpif(env === 'production', uglify())))
 								.pipe(gulp.dest('build/dev/js'));
 });
 

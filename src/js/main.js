3c055c8bc8af5273b9b2b7b01f9065975883b493
Aaron Ward <aaromp@gmail.com>
2015-02-12 18:17:34 -0800

    add event listeners

--- src/js/main.js
+++ src/js/main.js
@@ -10,13 +10,34 @@ for (var row = 1; row <= 9; row++) {
 		input.dataset.row = row;
 		input.dataset.column = column;
 		input.type = 'text';
-		input.setAttribute('pattern', '[0-9]*');
+		var exp = /[0-9]*/;
+		input.setAttribute('pattern', exp);
 		input.setAttribute('maxLength', 1);
 
 		tr.appendChild(td).appendChild(input);
 	}
 	table.appendChild(tr);
 }
+
+var previous;
+
+table.addEventListener('focusin', function(e) {
+	previous = e.target.value;
+	if (e.target.attributes.readonly === '') e.target.value = '';
+});
+
+table.addEventListener('focusout', function(e) {
+	if (e.target.value === '' && previous !== '') {
+		e.target.value = previous;
+	}
+});
+
+table.addEventListener('input', function(e) {
+	var row = e.target.dataset.row;
+	var column = e.target.dataset.column;
+	var value = e.target.value;
+	e.target.setAttribute('readonly', '');
+});
 
 document.body.appendChild(table);
 console.log(table);
